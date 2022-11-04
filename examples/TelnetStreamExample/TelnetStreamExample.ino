082e02bfe3ceece56bce4c25f3bfa87cc8cc7c81
Lennart Hennigs <mail@lennarthennigs.de>
2022-11-04 17:56:22 +0100

    stream example

---
+++ examples/TelnetStreamExample/TelnetStreamExample.ino
@@ -0,0 +1,68 @@
+/* ------------------------------------------------- */
+
+#include "ESPTelnetStream.h"
+
+/* ------------------------------------------------- */
+
+#define SERIAL_SPEED  9600
+#define INFRA_SSID    "MY SSID"
+#define INFRA_PSWD    "MY PASS"
+
+/* ------------------------------------------------- */
+
+ESPTelnetStream telnet;
+
+/* ------------------------------------------------- */
+
+void telnetConnected(String ip) {
+  Serial.print(ip);
+  Serial.println(" connected.");
+}
+
+void telnetDisconnected(String ip) {
+  Serial.print(ip);
+  Serial.println(" disconnected.");
+}
+
+void telnetReconnect(String ip) {
+  Serial.print(ip);
+  Serial.println(" reconnected.");
+}
+
+/* ------------------------------------------------- */
+
+void setup() {
+  Serial.begin(SERIAL_SPEED);
+  Serial.println("ESP Telnet Test");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(INFRA_SSID, INFRA_PSWD);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(100);
+  }
+
+  telnet.onConnect(telnetConnected);
+  telnet.onDisconnect(telnetDisconnected);
+  telnet.onReconnect(telnetReconnect);
+
+  Serial.print("Telnet.begin: ");
+  if(telnet.begin()) {
+    Serial.println("Successful");
+  } else {
+    Serial.println("Failed");
+  }
+}
+
+/* ------------------------------------------------- */
+
+void loop() {
+  telnet.loop();
+  if(Serial.available() > 0) {
+    telnet.write(Serial.read());
+  }
+  if (telnet.available() > 0) {    
+    Serial.print(char(telnet.read()));
+  }
+}
+
+/* ------------------------------------------------- */
